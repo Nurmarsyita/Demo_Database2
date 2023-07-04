247966ed1fb68ea23d6e5c73bebca26547e3ef10
22038005 <22038005@myrp.edu.sg>
2023-07-04 19:50:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demodatabase2/Task.java
@@ -0,0 +1,34 @@
+package com.example.demodatabase2;
+
+import androidx.annotation.NonNull;
+
+public class Task {
+
+    @NonNull
+    @Override
+    public String toString() {
+        return id + "\n" + description + "\n" + date;
+    }
+
+    private int id;
+    private String description;
+    private String date;
+
+    public Task(int id, String description, String date) {
+        this.id = id;
+        this.description = description;
+        this.date = date;
+    }
+
+    public int getId() { return id; }
+
+    public String getDescription() { return description; }
+
+    public String getDate() { return date;}
+
+
+
+
+}
+
+
